f62498fc25f29f97c8f5cc8730a629c6881d5a8d
GParfan <49314033+GParfan@users.noreply.github.com>
2019-06-03 03:08:19 -0300

    novas sprites

--- NovoJogo.py
+++ NovoJogo.py
@@ -16,6 +16,8 @@ GREEN  = (0, 255, 0)
 BLUE   = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+VIDA = 100
+
 GALHO_LISTA = [(450, HEIGHT - 140),
                (200, HEIGHT - 290),
                (200, HEIGHT - 440),
@@ -45,26 +47,48 @@ class Player(pygame.sprite.Sprite):
         if self.rect.left > WIDTH/2 + 50:
             self.rect.left = WIDTH/2 + 50
 
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h):
+class HealthBar(pygame.sprite.Sprite):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((w,h))
-        self.image.fill(BLUE)
+
+        self.image = pygame.Surface((100,20))
+        self.image.fill(RED)
+
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y    
+        self.rect.centerx = WIDTH/2
+        self.rect.y = 20
+
+        self.regen = 0
+
+    def update(self):
+        #healthWidth += self.regen
+        pass
 
 class Galho(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(path.join(img_dir,'Goomba.png')).convert_alpha()
-        self.image = pygame.transform.scale(self.image,(150,50))
+        self.image = pygame.image.load(path.join(img_dir,'Galho.png')).convert_alpha()
+        #self.image = pygame.transform.scale(self.image,(150,50))
         self.image.set_colorkey(BLACK)
         
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         
+    def update(self):
+        pass
+
+class Tronco(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(path.join(img_dir,'Tronco2.png')).convert_alpha()
+        #self.image = pygame.transform.scale(self.image,(100,600))
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 400
+        self.rect.y = 0
+
     def update(self):
         pass
 
@@ -76,8 +100,13 @@ pygame.display.set_caption("Timberman!!!")
 
 all_sprites = pygame.sprite.Group()
 galho = pygame.sprite.Group()
+
+tronco = Tronco()
+health = HealthBar()
 player = Player()
+all_sprites.add(tronco)
 all_sprites.add(player)
+all_sprites.add(health)
 
 for branch in GALHO_LISTA:
     g = Galho(*branch)
@@ -100,6 +129,7 @@ try:
                 if event.key == pygame.K_RIGHT:
                     player.pos = 200
 
+
                 if event.key == pygame.K_LEFT:
                     player.pos = -250
                     
@@ -115,10 +145,10 @@ try:
                 if event.key == pygame.K_RIGHT:
                     player.pos = 0
             
-        while len(galho) < 4:
+        if len(galho) < 4:
             lista_posicao = [200, 450]
             random.shuffle(lista_posicao)
-            g = Galho(lista_posicao[0], 10)
+            g = Galho(lista_posicao[0], 40)
             all_sprites.add(g)
             galho.add(g)
 
