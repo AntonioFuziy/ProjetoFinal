ec1bed1190b347debdd4b4acb82c58236c533ca8
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2019-06-03 16:45:36 -0300

    Adicionei Sprite do Personagem

--- NovoJogo.py
+++ NovoJogo.py
@@ -3,6 +3,7 @@ from os import path
 import random
 
 img_dir = path.join(path.dirname(__file__),'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
 
 WIDTH = 800
 HEIGHT = 600 
@@ -28,13 +29,13 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         
-        self.image = pygame.image.load(path.join(img_dir,'PersonagemTeste.png')).convert_alpha()
-        self.image = pygame.transform.scale(self.image,(100,100))
+        self.image = pygame.image.load(path.join(img_dir,'Pulica1.png')).convert_alpha()
+        self.image = pygame.transform.scale(self.image,(120,120))
         
         self.image.set_colorkey(BLACK)
         
         self.rect = self.image.get_rect()
-        self.rect.right = WIDTH/2 - 50
+        self.rect.right = WIDTH/2 - 70
         self.rect.y = HEIGHT - 100
         
         self.pos = 0
@@ -42,10 +43,25 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.pos
         
-        if self.rect.right < WIDTH/2 - 50:
-            self.rect.right = WIDTH/2 - 50
-        if self.rect.left > WIDTH/2 + 50:
-            self.rect.left = WIDTH/2 + 50
+        if self.rect.right < WIDTH/2 - 30:
+            self.rect.right = WIDTH/2 - 30
+        if self.rect.left > WIDTH/2 + 30:
+            self.rect.left = WIDTH/2 + 30
+        
+class Background(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        background = pygame.image.load(path.join(img_dir,'Background.jpg')).convert()
+        
+        self.image = background
+        self.image = pygame.transform.scale(background,(WIDTH,HEIGHT))
+        
+        self.image.set_colorkey(BLACK)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = HEIGHT
 
 class HealthBar(pygame.sprite.Sprite):
     def __init__(self):
@@ -98,12 +114,28 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Timberman!!!")
 
+background = pygame.image.load(path.join(img_dir, 'Background.jpg')).convert()
+background = pygame.transform.scale(background,(800,600))
+background_rect = background.get_rect()
+
+#background.rect.x = 1920 
+#background.rect.y = 1080
+
+#pygame.mixer.music.load(path.join(snd_dir, ''))
+#pygame.mixer.music.set_volume(0.4)
+#boom_sound = pygame.mixer.Sound(path.join(snd_dir, ''))
+#destroy_sound = pygame.mixer.Sound(path.join(snd_dir, ''))
+#pew_sound = pygame.mixer.Sound(path.join(snd_dir, ''))
+
 all_sprites = pygame.sprite.Group()
 galho = pygame.sprite.Group()
 
 tronco = Tronco()
 health = HealthBar()
 player = Player()
+#background = Background()
+
+#all_sprites.add(background)
 all_sprites.add(tronco)
 all_sprites.add(player)
 all_sprites.add(health)
@@ -127,11 +159,14 @@ try:
             
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    player.pos = 200
-
+                    player.pos = 170
+                    player.image = pygame.image.load(path.join(img_dir,'Pulica3_Invertido.png'))
+                    player.image = pygame.transform.scale(player.image,(120,120)) 
 
                 if event.key == pygame.K_LEFT:
-                    player.pos = -250
+                    player.pos = -220
+                    player.image = pygame.image.load(path.join(img_dir,'Pulica3.png'))
+                    player.image = pygame.transform.scale(player.image,(120,120))
                     
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     for branch in galho:
@@ -142,9 +177,14 @@ try:
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     player.pos = 0
+                    player.image = pygame.image.load(path.join(img_dir,'Pulica1.png'))
+                    player.image = pygame.transform.scale(player.image,(120,120))
+                    
                 if event.key == pygame.K_RIGHT:
                     player.pos = 0
-            
+                    player.image = pygame.image.load(path.join(img_dir,'Pulica1_Invertido.png'))
+                    player.image = pygame.transform.scale(player.image,(120,120))
+                    
         if len(galho) < 4:
             lista_posicao = [200, 450]
             random.shuffle(lista_posicao)
@@ -159,8 +199,8 @@ try:
             running = False
                     
         screen.fill(BLACK)
-#        screen.blit(background.image, background.rect)
-#        screen.blit(text, textRect)
+        screen.blit(background, background_rect)
+        #screen.blit(text, textRect)
         all_sprites.draw(screen)
         pygame.display.flip()
                     
